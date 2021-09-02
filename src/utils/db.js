7976840cfc38f79ab18aa35b6dcb50422d2b1474
Jose Arteaga <jarteaga00@mylangara.ca>
2021-09-01 20:57:32 -0700

    Add password hashing for employee, and email validation

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-const { format } = require('util')
 
 dotenv.config({ path: './.env' })
 
 const connect = (url = process.env.DB_URL, opts = {}) => {
-    return mongoose.connect(url, { ...opts, useNewUrlParser: true })
+    return mongoose.connect(url, {
+        ...opts,
+        useNewUrlParser: true
+    })
 }
 
 module.exports = { connect }
