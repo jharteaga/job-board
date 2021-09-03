38b97c1db77a54921a089a295301fa0aad366c6f
Jose Arteaga <jarteaga00@mylangara.ca>
2021-09-03 12:03:35 -0700

    Add sign in route, and validate password method in employee model

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -35,4 +35,40 @@ const signup = async (req, res) => {
     }
 }
 
-module.exports = { signup }
+const signin = async (req, res) => {
+    const { email, password } = req.body
+
+    // Validate if email and password were provided
+    if (!email || !password) {
+        return res
+            .status(400)
+            .json({ message: 'Email and password must be provided' })
+    }
+
+    try {
+        const employee = await Employee.findOne({
+            email: req.body.email
+        }).exec()
+
+        // Validate if employee exists
+        if (!employee)
+            return res.status(404).json({ message: 'Employee not found' })
+
+        const isPasswordValid = await employee.validatePassword(
+            req.body.password
+        )
+
+        // Validate if the given password matches
+        if (!isPasswordValid)
+            return res
+                .status(401)
+                .json({ message: 'Password provided is invalid' })
+
+        const token = newToken(employee)
+        return res.status(200).json({ token })
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+}
+
+module.exports = { signup, signin }
